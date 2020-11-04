cbbdd60057758f91776140d4efe0fe686b7e02b6
SitiLatifah <stlatifah257@gmail.com>
2020-11-04 07:45:32 -0800

    materi phyton

---
+++ LatihanBiodata.py
@@ -0,0 +1,15 @@
+print :"  ========================================" 
+print :"  Latihan Biodata Muhammad Rifqi Rizqullah    "
+print :"  ========================================"
+#variabel
+nama= input ("Masukan Nama Lengkap Anda: ")
+panggilan= input ("Masukan Nama Panggilan: ")
+nim= input ("Masukan Nim Anda: ")
+ttl= input ("Masukan Tempat Lahir Anda`: ")
+tl= input ("Masukan umur  Anda: ")
+telpone= input ("Masukan No Telpon Anda: ")
+alamat= input ("Masukan Alamat Anda: ")
+
+
+#Menampilkan Inputan User
+print (" Assalamu'alaikum Wr.Wb. ""Let me introduce my self My name is",nama,"but you can call me",panggilan,".My NIM is",nim,"I was born in",ttl,"and I am",tl,"years old. I am very glad if you want to invite my house in",alamat,".So, don't forget to call me before with the number",telpone,"\n Terimakasih Jangan Lupa Semangat")
